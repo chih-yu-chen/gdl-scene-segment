54396c9be27f621767b19820750299754076a697
Chih-Yu Chen <chihyuchentw@gmail.com>
2023-06-05 17:21:33 +0200

    use val as test; add label map

--- scannet_dataset.py
+++ scannet_dataset.py
@@ -23,16 +23,17 @@ class ScanNetDataset(Dataset):
         self.data_dir = Path(data_dir)
         self.k_eig = k_eig 
         self.op_cache_dir = Path(op_cache_dir)
+        self.classes = np.asarray([0,1,2,3,4,5,6,7,8,9,10,11,12,14,16,24,28,33,34,36,39])
+        self.label_map = np.zeros(41, dtype=np.int8)
+        np.put(self.label_map, self.classes, np.arange(self.classes.size))
 
         # load train test split
         split_dir = self.repo_dir/"Tasks"/"Benchmark"
         if self.train:
             with open(split_dir/"scannetv2_train.txt", 'r') as f:
                 self.scene_list = f.read().splitlines()
-            with open(split_dir/"scannetv2_val.txt", 'r') as f:
-                self.scene_list.extend(f.read().splitlines())
         else:
-            with open(split_dir/"scannetv2_test.txt", 'r') as f:
+            with open(split_dir/"scannetv2_val.txt", 'r') as f:
                 self.scene_list = f.read().splitlines()
 
     def __len__(self):
@@ -44,16 +45,15 @@ class ScanNetDataset(Dataset):
         scene = self.scene_list[idx]
 
         # get mesh and label paths
-        if self.train:    
-            train_dir = self.data_dir/"scans"
-            mesh_path = train_dir/scene/(scene+"_vh_clean_2.ply")
-            label_path = train_dir/scene/(scene+"_vh_clean_2.labels.ply")
-        else:
-            test_dir = self.data_dir/"scans_test"
-            mesh_path = test_dir/scene/(scene+"_vh_clean_2.ply")
+        # if self.train:
+        train_dir = self.data_dir/"scans"
+        mesh_path = train_dir/scene/(scene+"_vh_clean_2.ply")
+        label_path = train_dir/scene/(scene+"_vh_clean_2.labels.ply")
+        # else:
+        #     test_dir = self.data_dir/"scans_test"
+        #     mesh_path = test_dir/scene/(scene+"_vh_clean_2.ply")
 
         # load mesh
-        print(f"loading mesh {mesh_path}")
         verts, faces = pp3d.read_mesh(str(mesh_path))
         verts = torch.tensor(np.ascontiguousarray(verts)).float()
         faces = torch.tensor(np.ascontiguousarray(faces))
@@ -64,13 +64,14 @@ class ScanNetDataset(Dataset):
         # precompute operators
         frames, massvec, L, evals, evecs, gradX, gradY = diffusion_net.geometry.get_operators(verts, faces, self.k_eig, self.op_cache_dir)
 
-        if not self.train:
-            return verts, faces, frames, massvec, L, evals, evecs, gradX, gradY
+        # if not self.train:
+        #     return verts, faces, frames, massvec, L, evals, evecs, gradX, gradY
 
         # load labels
         with open(label_path, 'rb') as f:
             plydata = PlyData.read(f)
             labels = plydata['vertex'].data['label']
-        labels = torch.tensor(np.ascontiguousarray(labels.astype(np.int16)))
+        labels = self.label_map[labels]
+        labels = torch.tensor(np.ascontiguousarray(labels.astype(np.int64)))
 
         return verts, faces, frames, massvec, L, evals, evecs, gradX, gradY, labels
