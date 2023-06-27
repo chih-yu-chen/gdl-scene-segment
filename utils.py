92bab9691196666973244999ae1f702a7a5679a6
Chih-Yu Chen <chihyuchentw@gmail.com>
2023-06-27 12:28:51 +0200

    back-convert labels to nyu40; restrict gt visualization to 20 classes

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
+import numpy as np
+from pathlib import Path
+from plyfile import PlyData
 from torcheval.metrics import MulticlassConfusionMatrix
+
+
 
 def get_ious(preds, labels, n_class, device):
 
@@ -10,3 +15,43 @@ def get_ious(preds, labels, n_class, device):
     fns = mcm_results.sum(axis=0) - tps
 
     return tps, fps, fns
+
+def back_convert_labels(pred_dir, classes):
+
+    label_files = Path(pred_dir).glob("*.txt")
+    for file in label_files:
+        labels = np.loadtxt(file).astype(np.uint8)
+        # np.unique(labels, return_counts=True)
+        labels = classes[labels]
+        print(f"saving back-converted labels at {file}")
+        np.savetxt(file, labels, fmt='%d', delimiter='\n')
+
+def save_ground_truth_labels(scene_list, gt_dir, dst_dir, label_map):
+
+    for scene in scene_list:
+        label_path = gt_dir/scene/(scene+"_vh_clean_2.labels.ply")
+        with open(label_path, 'rb') as f:
+            labels = PlyData.read(f)['vertex'].data['label']
+        labels = label_map[labels]
+        dst_path = dst_dir/(scene+"_labels.txt")
+        print(f"saving ground truth labels at {dst_path}")
+        np.savetxt(dst_path, labels, fmt='%d', delimiter='\n')
+
+    return
+
+if __name__ == '__main__':
+
+    classes = np.asarray([0,1,2,3,4,5,6,7,8,9,10,11,12,14,16,24,28,33,34,36,39])
+    label_map = np.zeros(41, dtype=np.int8)
+    np.put(label_map, classes, np.arange(classes.size))
+
+    val_split = "ScanNet/Tasks/Benchmark/scannetv2_val.txt"
+    with open(val_split, 'r') as f:
+        scene_list = f.read().splitlines()
+
+    pred_dir = Path("/media/cychen/HDD/scannet/gts/")
+    back_convert_labels(pred_dir, classes)
+
+    # gt_dir = Path("/media/cychen/HDD/scannet/scans")
+    # dst_dir = Path("/media/cychen/HDD/scannet/gts")
+    # save_ground_truth_labels(scene_list, gt_dir, dst_dir, label_map)
--- visualize_labels_on_mesh.sh
+++ visualize_labels_on_mesh.sh
@@ -1,8 +1,19 @@
-readarray -t valset < ScanNet/Tasks/Benchmark/300000/scannetv2_val.txt
+#!/bin/bash
+PATH_SPLIT="ScanNet/Tasks/Benchmark/scannetv2_val.txt"
 
-for i in "${valset[@]}"; do
-	echo "copying ground truth $i"
-	cp /media/cychen/HDD/scannet/scans/"$i"/"$i"_vh_clean_2.labels.ply vis/gts/
-	echo "generating prediction visualization $i"
-	python2 ScanNet/BenchmarkScripts/3d_helpers/visualize_labels_on_mesh.py --pred_file /media/cychen/HDD/scannet/predictions/"$i"_labels.txt --mesh_file /media/cychen/HDD/scannet/scans/"$i"/"$i"_vh_clean_2.ply --output_file vis/predictions/"$i"_labels.ply
+readarray -t VALSET < $PATH_SPLIT
+
+for i in "${VALSET[@]}"; do
+
+	echo "generating ground truth visualization $i"
+	PATH_GT="/media/cychen/HDD/scannet/gts/"$i"_labels.txt"
+	PATH_MESH="/media/cychen/HDD/scannet/scans/"$i"/"$i"_vh_clean_2.ply"
+	PATH_OUT="vis/gts/"$i"_labels.ply"
+	python2 ScanNet/BenchmarkScripts/3d_helpers/visualize_labels_on_mesh.py --pred_file $PATH_GT --mesh_file $PATH_MESH --output_file $PATH_OUT
+
+	# echo "generating prediction visualization $i"
+	# PATH_PRED="/media/cychen/HDD/scannet/preds/hal_50_12/"$i"_labels.txt"
+	# PATH_MESH="/media/cychen/HDD/scannet/scans/"$i"/"$i"_vh_clean_2.ply"
+	# PATH_OUT="vis/preds/hal_50_12/"$i"_labels.ply"
+	# python2 ScanNet/BenchmarkScripts/3d_helpers/visualize_labels_on_mesh.py --pred_file $PATH_PRED --mesh_file $PATH_MESH --output_file $PATH_OUT
 done
