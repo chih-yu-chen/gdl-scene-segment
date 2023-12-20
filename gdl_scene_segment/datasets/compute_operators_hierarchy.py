3b3c6b78f240501615b9a988bd4eb275d7979033
Chih-Yu Chen <chihyuchentw@gmail.com>
2023-12-20 16:49:56 +0100

    compute_operators_hierarchy.py reverts to single process

--- gdl_scene_segment/datasets/compute_operators_hierarchy.py
+++ gdl_scene_segment/datasets/compute_operators_hierarchy.py
@@ -1,52 +1,15 @@
 import argparse
-from functools import partial
-import multiprocessing as mp
 import numpy as np
 from pathlib import Path
 import potpourri3d as pp3d
 import sys
 import torch
+from tqdm import tqdm
 
 pkg_path = Path(__file__).parents[2]/ "diffusion-net"/ "src"
 sys.path.append(str(pkg_path))
 import diffusion_net
 
-
-
-def process_frame(scene:str,
-                  data_dir:Path,
-                  hierarchy_dir:Path,
-                  op_cache_dir:Path,
-                  n_levels:int,
-                  k_eig:int):
-
-    print(f"Processing: {scene}")
-
-    # load mesh
-    mesh_path = data_dir/ "scenes"/ f"{scene}_vh_clean_2.ply"
-    mesh_paths = [hierarchy_dir/ "scenes"/ mesh_path.with_stem(f"{mesh_path.stem}_{i+1}").name
-                for i in range(n_levels)]
-    mesh_paths.insert(0, mesh_path)
-
-    verts = []
-    faces = []
-
-    for path in mesh_paths:
-        v, f = pp3d.read_mesh(path.as_posix())
-        verts.append(torch.tensor(np.ascontiguousarray(v)).float())
-        faces.append(torch.tensor(np.ascontiguousarray(f.astype(np.int32))))
-
-    # unit scale
-    scale = np.linalg.norm(verts[0], axis=-1).max()
-    verts = [v / scale for v in verts]
-
-    # precompute operators
-    for v, f in zip(verts, faces):
-        _ = diffusion_net.geometry.get_operators(v, f, k_eig, op_cache_dir)
-
-    print(f"Processed: {scene}")
-
-    return
 
 
 if __name__ == "__main__":
@@ -79,16 +42,26 @@ if __name__ == "__main__":
     with open(split_dir/ "scannetv2_test.txt", 'r') as f:
         scenes.extend(f.read().splitlines())
 
-    # Partial function
-    process_frame_p = partial(process_frame,
-                              data_dir=data_dir,
-                              hierarchy_dir=hierarchy_dir,
-                              op_cache_dir=op_cache_dir,
-                              n_levels=n_levels,
-                              k_eig=k_eig)
+    for scene in tqdm(scenes):
 
-    # multi-processing
-    pf_pool = mp.Pool(processes=16)
-    pf_pool.map(process_frame_p, scenes)
-    pf_pool.close()
-    pf_pool.join()
+        # load mesh
+        mesh_path = data_dir/ "scenes"/ f"{scene}_vh_clean_2.ply"
+        mesh_paths = [hierarchy_dir/ "scenes"/ mesh_path.with_stem(f"{mesh_path.stem}_{i+1}").name
+                    for i in range(n_levels)]
+        mesh_paths.insert(0, mesh_path)
+
+        verts = []
+        faces = []
+
+        for path in mesh_paths:
+            v, f = pp3d.read_mesh(path.as_posix())
+            verts.append(torch.tensor(np.ascontiguousarray(v)).float())
+            faces.append(torch.tensor(np.ascontiguousarray(f.astype(np.int32))))
+
+        # unit scale
+        scale = np.linalg.norm(verts[0], axis=-1).max()
+        verts = [v / scale for v in verts]
+
+        # precompute operators
+        for v, f in zip(verts, faces):
+            _ = diffusion_net.geometry.get_operators(v, f, k_eig, op_cache_dir)
