706245da3ab8ef3e0ed11be5cf0cf71cc6ba2ce8
Chih-Yu Chen <chihyuchentw@gmail.com>
2024-02-29 18:05:57 +0100

    add skip_linear; refactor all layers and some intermediate features

--- gdl_scene_segment/model/model.py
+++ gdl_scene_segment/model/model.py
@@ -212,15 +212,15 @@ class GeodesicBranch(nn.Module):
 
         # Unet-like structure
         #----- Input -----
-        # self.input_lin = nn.Linear(c_in, c0)
-        self.input_lin = nn.Sequential(
+        # self.input_linear = nn.Linear(c_in, c0)
+        self.input_linear = nn.Sequential(
             nn.Linear(c_in, c1),
             nn.ReLU()
         )
 
         #----- Encoder -----
         # # Level 0
-        # self.enc_0 = []
+        # self.enc_diffusion_0 = []
         # for i in range(n_diffnet_blocks):
         #     block = DiffNetLayers.DiffusionNetBlock(C_width=c0,
         #                                             mlp_hidden_dims=[c0]*n_mlp_hidden,
@@ -228,15 +228,15 @@ class GeodesicBranch(nn.Module):
         #                                             diffusion_method='spectral',
         #                                             with_gradient_features=True,
         #                                             with_gradient_rotations=True)
-        #     self.enc_0.append(block)
-        #     self.add_module(f"Encoder_L0_DiffusionNetBlock_{i}", self.enc_0[-1])
+        #     self.enc_diffusion_0.append(block)
+        #     self.add_module(f"Encoder_L0_DiffusionNetBlock_{i}", self.enc_diffusion_0[-1])
 
         # Level 1
-        # self.enc_lin_1 = nn.Sequential(
+        # self.enc_widen_1 = nn.Sequential(
         #     nn.Linear(c0, c1),
         #     nn.ReLU()
         # )
-        self.enc_1 = []
+        self.enc_diffusion_1 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c1,
                                                     mlp_hidden_dims=[c1]*n_mlp_hidden,
@@ -244,15 +244,15 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.enc_1.append(block)
-            self.add_module(f"Encoder_L1_DiffusionNetBlock_{i}", self.enc_1[-1])
+            self.enc_diffusion_1.append(block)
+            self.add_module(f"Encoder_L1_DiffusionNetBlock_{i}", self.enc_diffusion_1[-1])
 
         # Level 2
-        self.enc_lin_2 = nn.Sequential(
+        self.enc_widen_2 = nn.Sequential(
             nn.Linear(c1, c2),
             nn.ReLU()
         )
-        self.enc_2 = []
+        self.enc_diffusion_2 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c2,
                                                     mlp_hidden_dims=[c2]*n_mlp_hidden,
@@ -260,15 +260,15 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.enc_2.append(block)
-            self.add_module(f"Encoder_L2_DiffusionNetBlock_{i}", self.enc_2[-1])
+            self.enc_diffusion_2.append(block)
+            self.add_module(f"Encoder_L2_DiffusionNetBlock_{i}", self.enc_diffusion_2[-1])
 
         # Level 3
-        self.enc_lin_3 = nn.Sequential(
+        self.enc_widen_3 = nn.Sequential(
             nn.Linear(c2, c3),
             nn.ReLU()
         )
-        self.enc_3 = []
+        self.enc_diffusion_3 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c3,
                                                     mlp_hidden_dims=[c3]*n_mlp_hidden,
@@ -276,15 +276,15 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.enc_3.append(block)
-            self.add_module(f"Encoder_L3_DiffusionNetBlock_{i}", self.enc_3[-1])
+            self.enc_diffusion_3.append(block)
+            self.add_module(f"Encoder_L3_DiffusionNetBlock_{i}", self.enc_diffusion_3[-1])
 
         #----- Middle -----
-        self.enc_lin_m = nn.Sequential(
+        self.enc_widen_m = nn.Sequential(
             nn.Linear(c3, c_m),
             nn.ReLU()
         )
-        self.mid = []
+        self.mid_diffusion = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c_m,
                                                     mlp_hidden_dims=[c_m]*n_mlp_hidden,
@@ -292,20 +292,21 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.mid.append(block)
-            self.add_module(f"Middle_DiffusionNetBlock_{i}", self.mid[-1])
+            self.mid_diffusion.append(block)
+            self.add_module(f"Middle_DiffusionNetBlock_{i}", self.mid_diffusion[-1])
 
         #----- Decoder -----
         # Level 3
-        self.dec_lin_30 = nn.Sequential(
+        self.dec_narrow_3 = nn.Sequential(
             nn.Linear(c_m, c3),
             nn.ReLU()
         )
-        self.dec_lin_31 = nn.Sequential(
+        self.dec_halve_3 = nn.Sequential(
             nn.Linear(c3*2, c3),
             nn.ReLU()
         )
-        self.dec_3 = []
+        self.skip_linear_3 = nn.Linear(c3, c3, bias=False)
+        self.dec_diffusion_3 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c3,
                                                     mlp_hidden_dims=[c3]*n_mlp_hidden,
@@ -313,19 +314,20 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.dec_3.append(block)
-            self.add_module(f"Decoder_L3_DiffusionNetBlock_{i}", self.dec_3[-1])
+            self.dec_diffusion_3.append(block)
+            self.add_module(f"Decoder_L3_DiffusionNetBlock_{i}", self.dec_diffusion_3[-1])
 
         # Level 2
-        self.dec_lin_20 = nn.Sequential(
+        self.dec_narrow_2 = nn.Sequential(
             nn.Linear(c3, c2),
             nn.ReLU()
         )
-        self.dec_lin_21 = nn.Sequential(
+        self.dec_halve_2 = nn.Sequential(
             nn.Linear(c2*2, c2),
             nn.ReLU()
         )
-        self.dec_2 = []
+        self.skip_linear_2 = nn.Linear(c2, c2, bias=False)
+        self.dec_diffusion_2 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c2,
                                                     mlp_hidden_dims=[c2]*n_mlp_hidden,
@@ -333,19 +335,20 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.dec_2.append(block)
-            self.add_module(f"Decoder_L2_DiffusionNetBlock_{i}", self.dec_2[-1])
+            self.dec_diffusion_2.append(block)
+            self.add_module(f"Decoder_L2_DiffusionNetBlock_{i}", self.dec_diffusion_2[-1])
 
         # Level 1
-        self.dec_lin_10 = nn.Sequential(
+        self.dec_narrow_1 = nn.Sequential(
             nn.Linear(c2, c1),
             nn.ReLU()
         )
-        self.dec_lin_11 = nn.Sequential(
+        self.dec_halve_1 = nn.Sequential(
             nn.Linear(c1*2, c1),
             nn.ReLU()
         )
-        self.dec_1 = []
+        self.skip_linear_1 = nn.Linear(c1, c1, bias=False)
+        self.dec_diffusion_1 = []
         for i in range(n_diffnet_blocks):
             block = DiffNetLayers.DiffusionNetBlock(C_width=c1,
                                                     mlp_hidden_dims=[c1]*n_mlp_hidden,
@@ -353,19 +356,20 @@ class GeodesicBranch(nn.Module):
                                                     diffusion_method='spectral',
                                                     with_gradient_features=True,
                                                     with_gradient_rotations=True)
-            self.dec_1.append(block)
-            self.add_module(f"Decoder_L1_DiffusionNetBlock_{i}", self.dec_1[-1])
+            self.dec_diffusion_1.append(block)
+            self.add_module(f"Decoder_L1_DiffusionNetBlock_{i}", self.dec_diffusion_1[-1])
 
         # # Level 0
-        # self.dec_lin_00 = nn.Sequential(
+        # self.dec_narrow_0 = nn.Sequential(
         #     nn.Linear(c1, c0),
         #     nn.ReLU()
         # )
-        # self.dec_lin_01 = nn.Sequential(
+        # self.dec_halve_0 = nn.Sequential(
         #     nn.Linear(c0*2, c0),
         #     nn.ReLU()
         # )
-        # self.dec_0 = []
+        # self.skip_linear_0 = nn.Linear(c0, c0, bias=False)
+        # self.dec_diffusion_0 = []
         # for i in range(n_diffnet_blocks):
         #     block = DiffNetLayers.DiffusionNetBlock(C_width=c0,
         #                                             mlp_hidden_dims=[c0]*n_mlp_hidden,
@@ -373,12 +377,12 @@ class GeodesicBranch(nn.Module):
         #                                             diffusion_method='spectral',
         #                                             with_gradient_features=True,
         #                                             with_gradient_rotations=True)
-        #     self.dec_0.append(block)
-        #     self.add_module(f"Decoder_L0_DiffusionNetBlock_{i}", self.dec_0[-1])
+        #     self.dec_diffusion_0.append(block)
+        #     self.add_module(f"Decoder_L0_DiffusionNetBlock_{i}", self.dec_diffusion_0[-1])
 
         #----- Output -----
-        # self.output_lin = nn.Linear(c0, c_out)
-        self.output_lin = nn.Linear(c1, c_out)
+        # self.output_linear = nn.Linear(c0, c_out)
+        self.output_linear = nn.Linear(c1, c_out)
 
     def forward(self,
                 x_in,
@@ -390,92 +394,92 @@ class GeodesicBranch(nn.Module):
                 traces01, traces12, traces23, traces34):
 
         #----- Input -----
-        # x_0 = self.input_lin(x_in)
-        x_1 = self.input_lin(x_in)
+        # x_0 = self.input_linear(x_in)
+        x_1 = self.input_linear(x_in)
 
         #----- Encoder -----
         # # Level 0
-        # x_enc_0 = self.enc_0[0](x_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
-        # for block in self.enc_0[1:]:
-        #     x_enc_0 = block(x_enc_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
-        # x_enc_0 = x_enc_0 + x_0
-        # x_1, _ = scatter_max(x_enc_0, traces01, dim=-2)
+        # x_enc0 = self.enc_diffusion_0[0](x_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
+        # for block in self.enc_diffusion_0[1:]:
+        #     x_enc0 = block(x_enc0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
+        # x_enc0 = x_enc0 + x_0
+        # x_1, _ = scatter_max(x_enc0, traces01, dim=-2)
         
         # Level 1
-        # x_1 = self.enc_lin_1(x_1)
-        x_enc_1 = self.enc_1[0](x_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
-        for block in self.enc_1[1:]:
-            x_enc_1 = block(x_enc_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
-        x_enc_1 = x_enc_1 + x_1
-        x_2, _ = scatter_max(x_enc_1, traces12, dim=-2)
+        # x_1 = self.enc_widen_1(x_1)
+        x_enc1 = self.enc_diffusion_1[0](x_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
+        for block in self.enc_diffusion_1[1:]:
+            x_enc1 = block(x_enc1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
+        x_enc1 = x_enc1 + x_1
+        x_2, _ = scatter_max(x_enc1, traces12, dim=-2)
 
         # Level 2
-        x_2 = self.enc_lin_2(x_2)
-        x_enc_2 = self.enc_2[0](x_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
-        for block in self.enc_2[1:]:
-            x_enc_2 = block(x_enc_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
-        x_enc_2 = x_enc_2 + x_2
-        x_3, _ = scatter_max(x_enc_2, traces23, dim=-2)
+        x_2 = self.enc_widen_2(x_2)
+        x_enc2 = self.enc_diffusion_2[0](x_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
+        for block in self.enc_diffusion_2[1:]:
+            x_enc2 = block(x_enc2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
+        x_enc2 = x_enc2 + x_2
+        x_3, _ = scatter_max(x_enc2, traces23, dim=-2)
 
         # Level 3
-        x_3 = self.enc_lin_3(x_3)
-        x_enc_3 = self.enc_3[0](x_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
-        for block in self.enc_3[1:]:
-            x_enc_3 = block(x_enc_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
-        x_enc_3 = x_enc_3 + x_3
-        x_m, _ = scatter_max(x_enc_3, traces34, dim=-2)
+        x_3 = self.enc_widen_3(x_3)
+        x_enc3 = self.enc_diffusion_3[0](x_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
+        for block in self.enc_diffusion_3[1:]:
+            x_enc3 = block(x_enc3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
+        x_enc3 = x_enc3 + x_3
+        x_m, _ = scatter_max(x_enc3, traces34, dim=-2)
 
         #----- Middle -----
-        x_m = self.enc_lin_m(x_m)
-        x_mid = self.mid[0](x_m, mass_m, L_m, evals_m, evecs_m, gradX_m, gradY_m)
-        for block in self.mid[1:]:
+        x_m = self.enc_widen_m(x_m)
+        x_mid = self.mid_diffusion[0](x_m, mass_m, L_m, evals_m, evecs_m, gradX_m, gradY_m)
+        for block in self.mid_diffusion[1:]:
             x_mid = block(x_mid, mass_m, L_m, evals_m, evecs_m, gradX_m, gradY_m)
         x_mid = x_mid + x_m
 
         #----- Decoder -----
         # Level 3
         y_3 = x_mid[:,traces34,:]
-        y_3 = self.dec_lin_30(y_3)
-        y_3 = torch.cat((y_3, x_enc_3), dim=-1)
-        y_3 = self.dec_lin_31(y_3)
-        y_dec_3 = self.dec_3[0](y_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
-        for block in self.dec_3[1:]:
-            y_dec_3 = block(y_dec_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
-        y_dec_3 = y_dec_3 + y_3
+        y_3 = self.dec_narrow_3(y_3)
+        y_3 = torch.cat([y_3, x_enc3], dim=-1)
+        y_3 = self.dec_halve_3(y_3)
+        y_dec3 = self.dec_diffusion_3[0](y_3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
+        for block in self.dec_diffusion_3[1:]:
+            y_dec3 = block(y_dec3, mass_3, L_3, evals_3, evecs_3, gradX_3, gradY_3)
+        y_dec3 = y_dec3 + self.skip_linear_3(y_3)
 
         # Level 2
-        y_2 = y_dec_3[:,traces23,:]
-        y_2 = self.dec_lin_20(y_2)
-        y_2 = torch.cat((y_2, x_enc_2), dim=-1)
-        y_2 = self.dec_lin_21(y_2)
-        y_dec_2 = self.dec_2[0](y_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
-        for block in self.dec_2[1:]:
-            y_dec_2 = block(y_dec_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
-        y_dec_2 = y_dec_2 + y_2
+        y_2 = y_dec3[:,traces23,:]
+        y_2 = self.dec_narrow_2(y_2)
+        y_2 = torch.cat([y_2, x_enc2], dim=-1)
+        y_2 = self.dec_halve_2(y_2)
+        y_dec2 = self.dec_diffusion_2[0](y_2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
+        for block in self.dec_diffusion_2[1:]:
+            y_dec2 = block(y_dec2, mass_2, L_2, evals_2, evecs_2, gradX_2, gradY_2)
+        y_dec2 = y_dec2 + self.skip_linear_2(y_2)
 
         # Level 1
-        y_1 = y_dec_2[:,traces12,:]
-        y_1 = self.dec_lin_10(y_1)
-        y_1 = torch.cat((y_1, x_enc_1), dim=-1)
-        y_1 = self.dec_lin_11(y_1)
-        y_dec_1 = self.dec_1[0](y_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
-        for block in self.dec_1[1:]:
-            y_dec_1 = block(y_dec_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
-        y_dec_1 = y_dec_1 + y_1
+        y_1 = y_dec2[:,traces12,:]
+        y_1 = self.dec_narrow_1(y_1)
+        y_1 = torch.cat([y_1, x_enc1], dim=-1)
+        y_1 = self.dec_halve_1(y_1)
+        y_dec1 = self.dec_diffusion_1[0](y_1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
+        for block in self.dec_diffusion_1[1:]:
+            y_dec1 = block(y_dec1, mass_1, L_1, evals_1, evecs_1, gradX_1, gradY_1)
+        y_dec1 = y_dec1 + self.skip_linear_1(y_1)
 
         # # Level 0
-        # y_0 = y_dec_1[:,traces01,:]
-        # y_0 = self.dec_lin_00(y_0)
-        # y_0 = torch.cat((y_0, x_enc_0), dim=-1)
-        # y_0 = self.dec_lin_01(y_0)
-        # y_dec_0 = self.dec_0[0](y_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
-        # for block in self.dec_0[1:]:
-        #     y_dec_0 = block(y_dec_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
-        # y_dec_0 = y_dec_0 + y_0
+        # y_0 = y_dec1[:,traces01,:]
+        # y_0 = self.dec_narrow_0(y_0)
+        # y_0 = torch.cat([y_0, x_enc0], dim=-1)
+        # y_0 = self.dec_halve_0(y_0)
+        # y_dec0 = self.dec_diffusion_0[0](y_0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
+        # for block in self.dec_diffusion_0[1:]:
+        #     y_dec0 = block(y_dec0, mass_0, L_0, evals_0, evecs_0, gradX_0, gradY_0)
+        # y_dec0 = y_dec0 + self.skip_linear_0(y_0)
 
         #----- Output -----
-        # y = self.output_lin(y_dec_0)
-        y = self.output_lin(y_dec_1)
+        # y = self.output_linear(y_dec0)
+        y = self.output_linear(y_dec1)
 
         return y
 
