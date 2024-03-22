760d8e6de67fd5a44acd44bd3fc91aab984434ed
Chih-Yu Chen <chihyuchentw@gmail.com>
2024-03-22 15:00:38 +0100

    fix fps & fns

--- gdl_scene_segment/model/utils.py
+++ gdl_scene_segment/model/utils.py
@@ -65,7 +65,7 @@ def get_ious(preds, labels, n_class):
     mcm.update(preds, labels)
     mcm_results = mcm.compute()
     tps = mcm_results.diagonal()
-    fps = mcm_results.sum(axis=1) - tps
-    fns = mcm_results.sum(axis=0) - tps
+    fps = mcm_results.sum(axis=0) - tps
+    fns = mcm_results.sum(axis=1) - tps
 
     return tps, fps, fns
