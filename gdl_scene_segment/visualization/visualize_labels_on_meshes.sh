b664e6c36d8e8fa63fca48e4c74ca9aa328ce3d9
Chih-Yu Chen <chihyuchentw@gmail.com>
2024-04-09 11:04:40 +0200

    modify visualize_scenes.py for qualitative results

--- gdl_scene_segment/visualization/visualize_labels_on_meshes.sh
+++ gdl_scene_segment/visualization/visualize_labels_on_meshes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PATH_SPLIT="ScanNet/Tasks/Benchmark/scannetv2_val.txt"
-RUN_NAME="hal_20_10"
-mkdir "vis/preds/"$RUN_NAME
+PATH_SPLIT="gdl_scene_segment/datasets/splits/scannetv2_val.txt"
+RUN_NAME="03_baseline_noGradientRotation"
+mkdir "visualizations/pred_scenes/"$RUN_NAME
 
 readarray -t VALSET < $PATH_SPLIT
 
@@ -14,9 +14,9 @@ for i in "${VALSET[@]}"; do
 	# python2 ScanNet/BenchmarkScripts/3d_helpers/visualize_labels_on_mesh.py --pred_file $PATH_GT --mesh_file $PATH_MESH --output_file $PATH_OUT
 
 	echo "generating prediction visualization $i"
-	PATH_PRED="/media/cychen/HDD/scannet/preds/"$RUN_NAME"/"$i"_labels.txt"
+	PATH_PRED="experiments/baseline/"$RUN_NAME"/preds/"$i".txt"
 	PATH_MESH="/media/cychen/HDD/scannet/scans/"$i"/"$i"_vh_clean_2.ply"
-	PATH_OUT="vis/preds/"$RUN_NAME"/"$i"_labels.ply"
-	python3 visualize_labels_on_mesh.py --pred_file $PATH_PRED --mesh_file $PATH_MESH --output_file $PATH_OUT
+	PATH_OUT="visualizations/pred_scenes/"$RUN_NAME"/"$i".ply"
+	python3 gdl_scene_segment/visualization/visualize_labels_on_mesh.py --pred_file $PATH_PRED --mesh_file $PATH_MESH --output_file $PATH_OUT
 	
 done
